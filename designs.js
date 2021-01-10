cac7eef152f6c99d53899d660176c2a7d363a355
shearda <76824868+shearda@users.noreply.github.com>
2021-01-10 11:55:00 -0500

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,35 +1,39 @@
 // Select color input
-let inputColor = document.getElementById ("colorPicker");
+let inputColor = document.getElementById("colorPicker");
 
 // Select size input
-let table = document.getElementById("pixelCanvas");
-let iHeight = document.getElementById ("inputHeight");
-let iWidth = document.getElementById ("inputWidth");
+let tableCanvas = document.getElementById("pixelCanvas");
+let iHeight = document.getElementById("inputHeight");
+let iWidth = document.getElementById("inputWidth");
 
 
 // Make the grid
 let sPicker = document.getElementById("sizePicker");
-sPicker.addEventListener("submit", function(event) {
-  event.preventDefault();
-  makeGrid()
+sPicker.addEventListener("submit", function (event) {
+    event.preventDefault();
+    makeGrid()
 });
 
 
 // When size is submitted by the user, call makeGrid()
 function makeGrid() {
-  const height = iHeight.value;
-  const width = iWidth.value;
-  for (var w = 0; w < width; w++){
-    const row = table.insertRow();
-    for (var h = 0; h < height; h++){
-      const cell = row.insertCell();
+  let table = document.createElement('table')
+    const height = iHeight.value;
+    const width = iWidth.value;
+    for (let w = 0; w < width; w++) {
+        const row = table.insertRow();
+        for (let h = 0; h < height; h++) {
+            const cell = row.insertCell();
+            cell.addEventListener("click",event=>{
+                event.preventDefault();
+                event.target.style.backgroundColor = inputColor.value
+            })
+        }
     }
-  }
-  let cPicker = document.getElementsByClassName("cell");
-  cPicker.addEventListener("click", function (event) {
-    event.preventDefault();
-    cell.style.backgroundColor = inputColor;
-    document.appendChild("cell");
-    table.innerHTML = grid;
-  });
+    let children = tableCanvas.childNodes? tableCanvas.childNodes:[]
+    if(children && children.length===1){
+        tableCanvas.replaceChild(table,children[0])
+    }else{
+        tableCanvas.append(table)
+    }
 }
