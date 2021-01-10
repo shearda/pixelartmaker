c8f53a02dd89e64b375b436ac835655ad3db8243
alexanderhess <Hess.alexander1@web.de>
2021-01-10 12:25:34 +0100

    Fixed Color Picker and Reset Grid

--- designs.js
+++ designs.js
@@ -2,7 +2,7 @@
 let inputColor = document.getElementById ("colorPicker");
 
 // Select size input
-let table = document.getElementById("pixelCanvas");
+let tableCanvas = document.getElementById("pixelCanvas");
 let iHeight = document.getElementById ("inputHeight");
 let iWidth = document.getElementById ("inputWidth");
 
@@ -17,19 +17,27 @@ sPicker.addEventListener("submit", function(event) {
 
 // When size is submitted by the user, call makeGrid()
 function makeGrid() {
+  let table = document.createElement('table')
+
   const height = iHeight.value;
   const width = iWidth.value;
   for (var w = 0; w < width; w++){
     const row = table.insertRow();
     for (var h = 0; h < height; h++){
       const cell = row.insertCell();
+      cell.addEventListener("click", event=>{
+        event.preventDefault();
+        event.target.style.backgroundColor = inputColor.value;
+      })
     }
   }
-  let cPicker = document.getElementsByClassName("cell");
-  cPicker.addEventListener("click", function (event) {
-    event.preventDefault();
-    cell.style.backgroundColor = inputColor;
-    document.appendChild("cell");
-    table.innerHTML = grid;
-  });
+  let children = tableCanvas.childNodes? tableCanvas.childNodes: []
+  if(children && children.length == 1){
+    tableCanvas.replaceChild(table, children[0])
+  }else{
+    tableCanvas.append(table)
+  }
 }
+
+
+
